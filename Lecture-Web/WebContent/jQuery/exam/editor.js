742b590ba13f1e81d1022dc63ee2169d88fcbd4e
임규철 <@bit>
2017-06-09 10:52:34 +0900

    # jquery 끝

--- Lecture-Web/WebContent/jQuery/exam/editor.js
+++ Lecture-Web/WebContent/jQuery/exam/editor.js
@@ -1,30 +1,143 @@
 $(function() {
-	$('#btnSelectAll').click(function() {
-		getSelectOffset();
+
+	$('#textDiv').attr("spellcheck", false);
+	$('#textDiv').keydown(function(e) {
+		if (e.keyCode === 13) {
+			document.execCommand('insertHTML', false, '<br><br>');
+			return false;
+		}
 	});
 
+	$('#textDiv').focus(function() {
+		$('#textDiv').css('border', '3px solid steelblue');
+	});
+
+	$('#textDiv').blur(function() {
+		$('#textDiv').css('border', '2px solid gray');
+	});
+
+	$('#btnSelectAll').click(function() {
+		$('#textDiv').focus();
+		document.execCommand('selectAll', false, null);
+	});
+
+	$('#btnclearAll').click(function() {
+		$('#textDiv').empty();
+		$('#textDiv').focus();
+	});
+
+	$('#btnclearAttr').click(function() {
+		$('#textDiv').html(makePureText());
+	});
+});
+
+function makePureText() {
+	// $('#textDiv *').css('background', '');
+	$('#textDiv *').removeAttr('style');
+	var regex = /(<([^(br)^>]+)>)/ig;
+	var htmlText = $('#textDiv').html();
+	var pureStr = htmlText.replace(regex, "");
+	return pureStr;
+}
+
+/**
+ * font style
+ */
+
+$(function() {
 	$('#btnBold').click(function() {
-		var offsets = getSelectOffset();
-		var text = $('#textdiv').html();
-		// substring 전후에 태그를 넣음
-		var repStr = "메롱";
-		text = text.replace(text.substring(offsets[0], offsets[1]), repStr);
-		// [0]부터 ~까지,  [1]부터 마지막까지
-		// 그 사이의 문자를 태그로감싸서 다시 집어넣는다.,..
-		$('#textdiv').html(text);
+		fontStyle("bold");
+	});
+
+	$('#btnItalic').click(function() {
+		fontStyle("italic");
+	})
+
+	$('#btnUnderline').click(function() {
+		fontStyle("underline");
+	});
+});
+
+/**
+ * textArea align
+ */
+
+$(function() {
+	$('#btnAlignCenter').click(function() {
+		$('#textDiv').attr('align', 'center');
+	});
+
+	$('#btnAlignLeft').click(function() {
+		$('#textDiv').attr('align', 'left');
+	});
+
+	$('#btnAlignRight').click(function() {
+		$('#textDiv').attr('align', 'right');
 	});
 
 });
 
-function getSelectOffset() {
-	var selected = window.getSelection();
-	var offsets = [ selected.focusOffset, selected.baseOffset ];
-	// 시작과 끝(긁기 시작한 순간이 focus임 오름차순 정렬해야함)
-	offsets.sort(function(left, right) {
-		return left - right;
-	});
-	alert(offsets);
-	return offsets;
-}
+/**
+ * select Option event
+ */
 
-//http://pritomkumar.blogspot.kr/2013/06/how-to-get-selecteduser-highlighted.html
+$(function() {
+	$('#fontFamily').change(function() {
+		var fontFamily = $('#fontFamily option:selected').val();
+		if (fontFamily !== "") {
+			fontStyle('fontFamily', fontFamily);
+		}
+		$("#fontFamily").val("").attr("selected", "selected");
+	});
+
+	$('#fontSize').change(function() {
+		var size = $('#fontSize option:selected').val();
+		if (size !== "") {
+			fontStyle('size', size);
+		}
+		$("#fontSize").val("").attr("selected", "selected");
+	});
+
+	$('#fontColor').change(function() {
+		var color = $('#fontColor option:selected').val();
+		if (color !== "") {
+			fontStyle('color', color);
+		}
+		$("#fontColor").val("").attr("selected", "selected");
+	});
+
+	$('#fontBackground').change(function() {
+		var background = $('#fontBackground option:selected').val();
+		if (background !== "") {
+			fontStyle('background', background);
+		}
+		$("#fontBackground").val("").attr("selected", "selected");
+	});
+});
+
+function fontStyle(type, value) {
+	var range = window.getSelection().getRangeAt(0);
+	if (range === "")
+		return;
+	var span = document.createElement('span');
+
+	if (type === 'bold') {
+		span.style.fontWeight = 'bold';
+	} else if (type === 'italic') {
+		span.style.fontStyle = 'italic';
+	} else if (type === 'underline') {
+		span.style.textDecoration = 'underline';
+	} else if (type === 'size') {
+		span.style.fontSize = value + 'pt';
+	} else if (type === 'color') {
+		span.style.color = value;
+	} else if (type === 'background') {
+		span.style.backgroundColor = value;
+	} else if (type === 'fontFamily') {
+		span.style.fontFamily = value;
+	}
+
+	// extractContents();
+	span.appendChild(range.extractContents());
+	range.insertNode(span);
+}
